5f4c36088eda1dfeb244cf6db2015f66a1bf458b
Dio Arayan <drarayan1994@gmail.com>
2023-03-10 16:19:02 +0800

    security features added

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :articles
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
   resources :users, except: [:new]
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
 
